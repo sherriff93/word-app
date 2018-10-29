2945c0d8398fc6ce0eab00b7cb388058bfc305bb
Chris Sherriff <chrissherriffweb@gmail.com>
2018-10-29 11:38:02 +0000

    backup

---
+++ app/client/src/components/Popup.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import {Link} from "react-router-dom"
+import {hideCurrentPopup} from "../actions/actions"
+import connect from "react-redux/es/connect/connect"
+
+class Popup extends Component {
+    render() {
+
+        const PopupOuter = styled.div`
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            background-color: rgba(0,0,0, 0.5);
+        `
+
+        const PopupInner = styled.div`
+            position: absolute;
+            left: 25%;
+            right: 25%;
+            top: 25%;
+            bottom: 25%;
+            margin: auto;
+            background: white;
+        `
+
+        return (
+            <PopupOuter>
+                <PopupInner>
+                    <h1>{this.props.text}</h1>
+                    <button onClick={this.props.hideCurrentPopup}>close me</button>
+                </PopupInner>
+            </PopupOuter>
+        );
+    }
+
+    hideCurrentPopup() {
+        hideCurrentPopup()
+    }
+}
+
+const mapDispatchToProps = function(dispatch) {
+    return {
+        hideCurrentPopup: () => {
+            dispatch(hideCurrentPopup())
+        }
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(Popup)
--- app/client/src/reducers/app-reducer.js
+++ app/client/src/reducers/app-reducer.js
@@ -5,36 +5,38 @@ import Home from '../components/Home'
 import DictionaryMain from '../components/DictionaryMain'
 
 const initialState = {
-  dictionaries: [
-    {
-      path: '/',
-      exact: true,
-      name: 'Home',
-      main: Home
-    },
-    {
-      path: '/harry-potter',
-      name: 'Harry Potter',
-      main: ({match}) => (<DictionaryMain name="Harry Potter" match={match} />)
-    },
-    {
-      path: '/things-ive-heard',
-      name: 'Things I\'ve heard',
-      main: ({match}) => (<DictionaryMain name="Things I've heard" match={match} />)
-    },
-    {
-      path: '/woofing',
-      name: 'Woofing',
-      main: ({match}) => (<DictionaryMain name='Woofing' match={match} />)
-    }
-  ]
+    dictionaries: [
+        {
+            path: '/',
+            exact: true,
+            name: 'Home',
+            main: Home
+        },
+        {
+            path: '/harry-potter',
+            name: 'Harry Potter',
+            main: ({match}) => (<DictionaryMain name="Harry Potter" match={match}/>)
+        },
+        {
+            path: '/things-ive-heard',
+            name: 'Things I\'ve heard',
+            main: ({match}) => (<DictionaryMain name="Things I've heard" match={match}/>)
+        },
+        {
+            path: '/woofing',
+            name: 'Woofing',
+            main: ({match}) => (<DictionaryMain name='Woofing' match={match}/>)
+        }
+    ],
+    showPopupWithParams: null
 }
 
-const AppReducer = function(state = initialState, action) {
+const AppReducer = function (state = initialState, action) {
     let newState = null
-    let {dictionaries} = state
-    switch(action.type) {
-        case types.ADD_DICTIONARY:
+    let dictionary = null
+    let {dictionaries, showPopupWithParams} = state
+    switch (action.type) {
+        case types.ADD_DICTIONARY: {
             const {value} = action
             const valueCapped = value.charAt(0).toUpperCase() + value.slice(1)
             newState = {
@@ -42,16 +44,32 @@ const AppReducer = function(state = initialState, action) {
                 dictionaries: [...dictionaries, {
                     path: '/' + encodeURI(value),
                     name: valueCapped,// TODO need to format the value (caps first here, no space above) and caps below
-                    main: ({match}) => (<DictionaryMain name={valueCapped} match={match} />)
+                    main: ({match}) => (<DictionaryMain name={valueCapped} match={match}/>)
                 }]
             }
             break
-        case types.DELETE_DICTIONARY:
-            dictionaries = dictionaries.filter(function(dictionary, index) {
-              return action.value !== dictionary.name
+        }
+        case types.DELETE_DICTIONARY: {
+            dictionaries = dictionaries.filter(function (dictionary, index) {
+                return action.value !== dictionary.name
             })
             newState = {...state, dictionaries}
             break
+        }
+        case types.SHOW_EDIT_DICTIONARY_POPUP: {
+            const {dictionary} = action
+            showPopupWithParams = {
+                type: 'edit_dictionary',
+                dictionary: dictionary
+            }
+            newState = {...state, showPopupWithParams}
+            break
+        }
+        case types.HIDE_CURRENT_POPUP: {
+            showPopupWithParams = null
+            newState = {...state, showPopupWithParams}
+            break
+        }
         default:
             newState = state
     }
