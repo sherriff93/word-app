2a05b170d1c8c85f173220813b7520f0b6cbe258
Chris Sherriff <chrissherriffweb@gmail.com>
2018-12-12 07:19:39 +0000

    Added firebase dev config

--- app/client/src/firebase/firebase.js
+++ app/client/src/firebase/firebase.js
@@ -1,7 +1,16 @@
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
-const config = {
+const prodConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "word-app-prod.firebaseapp.com",
+    databaseURL: "https://word-app-prod.firebaseio.com",
+    projectId: "word-app-prod",
+    storageBucket: "word-app-prod.appspot.com",
+    messagingSenderId: "653407271697"
+};
+
+const devConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "word-app-7d570.firebaseapp.com",
     databaseURL: "https://word-app-7d570.firebaseio.com",
@@ -9,6 +18,10 @@ const config = {
     storageBucket: "word-app-7d570.appspot.com",
     messagingSenderId: "150918338866"
 };
+
+const config = process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
 
 if (!firebase.apps.length) {
     firebase.initializeApp(config);
